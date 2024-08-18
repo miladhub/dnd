77dff121ca01235310dcfb519456c9ba46e68fad
Milad Bourhani <milad.bourhani@gmail.com>
2024-08-18 16:56:53 +0200

    melf

--- src/main/java/org/meh/dnd/Spells.java
+++ src/main/java/org/meh/dnd/Spells.java
@@ -10,6 +10,13 @@ public class Spells
     public static List<DamageRoll> damageRollsFor(Spell s) {
         if (s.name().equals(MAGIC_MISSILE.name())) {
             return List.of(
+                    new DamageRoll(Dice.roll(1, s.damage(), 1), s.damage(), INT),
+                    new DamageRoll(Dice.roll(1, s.damage(), 1), s.damage(), INT),
+                    new DamageRoll(Dice.roll(1, s.damage(), 1), s.damage(), INT)
+            );
+        } else if (s.name().equals(MELF_ARROW.name())) {
+            return List.of(
+                    new DamageRoll(Dice.roll(1, s.damage(), 0), s.damage(), INT),
                     new DamageRoll(Dice.roll(1, s.damage(), 0), s.damage(), INT),
                     new DamageRoll(Dice.roll(1, s.damage(), 0), s.damage(), INT),
                     new DamageRoll(Dice.roll(1, s.damage(), 0), s.damage(), INT)
