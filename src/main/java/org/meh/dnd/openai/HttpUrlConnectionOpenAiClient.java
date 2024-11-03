31ab3317088d1b982863e05c8955f5e7f78b48df
Milad Bourhani <milad.bourhani@gmail.com>
2024-11-03 11:32:09 +0100

    using java 11 httpclient, improved logging, increased number of tokens

--- src/main/java/org/meh/dnd/openai/HttpUrlConnectionOpenAiClient.java
+++ src/main/java/org/meh/dnd/openai/HttpUrlConnectionOpenAiClient.java
@@ -2,27 +2,31 @@ package org.meh.dnd.openai;
 
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
 import java.net.URI;
-import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class HttpUrlConnectionOpenAiClient
         implements OpenAiClient
 {
     private static final Logger LOG = LoggerFactory.getLogger(OpenAiClient.class);
+    private static final String OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
     private static final String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
     private static final String OPENAI_CHAT_MODEL = "gpt-4o-mini";
-    private static final int OPENAI_RESPONSE_MAX_TOKENS = 500;
+    private static final int OPENAI_RESPONSE_MAX_TOKENS = 5000;
+    private final JsonbConfig jsonbConfig =
+            new JsonbConfig().withFormatting(true);
+    private final HttpClient httpClient = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(300))
+            .build();
 
     @Override
     public ChatResponse chatCompletion(
@@ -30,46 +34,42 @@ public class HttpUrlConnectionOpenAiClient
             List<ModelFunction> functions
     )
     throws Exception {
-        try (Jsonb jsonb = JsonbBuilder.create()) {
+        try (Jsonb jsonb = JsonbBuilder.create(jsonbConfig)) {
             String json = toJson(messages, functions);
-            LOG.info("Request:\n{}", json);
-            HttpURLConnection con = getHttpURLConnection();
-            try (OutputStream os = con.getOutputStream()) {
-                LOG.debug("Sending JSON to chat completion API:\n{}", json);
-                byte[] bytes = json.getBytes(UTF_8);
-                os.write(bytes, 0, bytes.length);
-            }
-            try (InputStream is = con.getInputStream()) {
-                String res = new String(is.readAllBytes(), UTF_8);
-                LOG.info("Response:\n{}", res);
-                OpenAiResponse resp = jsonb.fromJson(res, OpenAiResponse.class);
-                OpenAiChoice choice = resp.choices().getFirst();
-                if (!"stop".equals(choice.finish_reason()))
-                    LOG.info("Finish reason: {}", choice.finish_reason());
-                LOG.debug("Usage: {}", resp.usage());
-                OpenAiResponseMessage msg = choice.message();
-                if (msg.function_call() != null)
-                    return new ChatResponse.FunctionCallChatResponse(
-                            msg.function_call().name(),
-                            msg.function_call().arguments()
-                    );
-                else return new ChatResponse.MessageChatResponse(msg.content());
+            LOG.debug("Request:\n{}", json);
+            LOG.info("Request content:\n{}",
+                    messages.stream()
+                            .map(OpenAiRequestMessage::content)
+                            .collect(Collectors.joining("\n")));
+
+            LOG.debug("Sending JSON to chat completion API:\n{}", json);
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(OPENAI_API_URL))
+                    .POST(HttpRequest.BodyPublishers.ofString(json))
+                    .header("Content-Type", "application/json")
+                    .header("Accept", "application/json")
+                    .header("Authorization", "Bearer " + OPENAI_API_KEY)
+                    .build();
+            HttpResponse<String> response = httpClient.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+            String res = response.body();
+            LOG.debug("Response:\n{}", res);
+            OpenAiResponse resp = jsonb.fromJson(res, OpenAiResponse.class);
+            OpenAiChoice choice = resp.choices().getFirst();
+            if (!"stop".equals(choice.finish_reason()))
+                LOG.info("Finish reason: {}", choice.finish_reason());
+            LOG.debug("Usage: {}", resp.usage());
+            OpenAiResponseMessage msg = choice.message();
+            if (msg.function_call() != null) {
+                return new ChatResponse.FunctionCallChatResponse(
+                        msg.function_call().name(),
+                        msg.function_call().arguments()
+                );
+            } else {
+                LOG.info("Response content:\n{}", msg.content());
+                return new ChatResponse.MessageChatResponse(msg.content());
             }
         }
-    }
-
-    private static HttpURLConnection getHttpURLConnection()
-    throws IOException {
-        URL url = URI.create("https://api.openai.com/v1/chat/completions").toURL();
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setDoOutput(true);
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setRequestProperty("Accept", "application/json");
-        con.setRequestProperty("Authorization", "Bearer " + OPENAI_API_KEY);
-        con.setConnectTimeout(300_000);
-        con.setReadTimeout(300_000);
-        return con;
     }
 
     private String toJson(
@@ -77,7 +77,7 @@ public class HttpUrlConnectionOpenAiClient
             List<ModelFunction> functions
     )
     throws Exception {
-        try (Jsonb jsonb = JsonbBuilder.create()) {
+        try (Jsonb jsonb = JsonbBuilder.create(jsonbConfig)) {
             if (functions.isEmpty()) {
                 return String.format(
                         """
